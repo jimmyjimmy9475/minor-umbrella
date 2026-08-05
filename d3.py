853512cb6c62025de143c01d66897d7f762bf07f
tsigabd2015-rgb <tsigabd2015@gmail.com>
2026-08-05 15:09:04 +1200

    Printed summary stats and saved to new csv

--- d3.py
+++ d3.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 import pandas as pd
 
@@ -21,7 +20,7 @@ dfs = []
 
 for month, month_year in months.items():
     # Read the file
-    df = pd.read_csv(f"listings_{month}.csv")
+    df = pd.read_csv(f"airbnb_data/listings_{month}.csv")
 
     # Clean neighbourhood_group
     df["neighbourhood_group"] = (
@@ -47,3 +46,12 @@ df_chch_all = pd.concat(dfs, ignore_index=True)
 print(df_chch_all[["month_year", "neighbourhood_group"]].head())
 
 print(df_chch_all.tail())
+
+print(df_chch_all.describe())
+
+# Print number of missing values in each column
+print(df_chch_all.isnull().sum())
+
+print(df_chch_all[['price', 'minimum_nights', 'number_of_reviews', 'availability_365', 'calculated_host_listings_count', 'number_of_reviews_ltm']].describe())
+
+df_chch_all.to_csv("listings_concat.csv")
