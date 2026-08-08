0af6b3de5b0730c6c22db65d529ea94f8d61b310
tsigabd2015-rgb <tsigabd2015@gmail.com>
2026-08-09 01:37:24 +1200

    Update d3.py

--- d3.py
+++ d3.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-
-import pandas as pd
 from datetime import datetime as dt
 
 months = {
@@ -23,15 +21,6 @@ for month, month_year in months.items():
     # Read the file
     df = pd.read_csv(f"airbnb_data/listings_{month}.csv")
 
-    # Clean neighbourhood_group
-    df["neighbourhood_group"] = (
-        df["neighbourhood_group"]
-        .astype(str)
-        .str.strip()
-        .str.replace(r"\s+", " ", regex=True)
-        .str.title()
-    )
-
     # Filter Christchurch City
     df_chch = df[df["neighbourhood_group"] == "Christchurch City"].copy()
 
@@ -45,18 +34,59 @@ df_chch_all = pd.concat(dfs, ignore_index=True)
 
 # Check
 print(df_chch_all[["month_year", "neighbourhood_group"]].head())
-
 print(df_chch_all.tail())
-
-print(df_chch_all.describe())
+print(df_chch_all["neighbourhood_group"].unique())
 
 # Print number of missing values in each column
 print(df_chch_all.isnull().sum())
 
-print(df_chch_all[['price', 'minimum_nights', 'number_of_reviews', 'availability_365', 'calculated_host_listings_count', 'number_of_reviews_ltm']].describe())
+# print summary statistics for the selected columns
+print(df_chch_all[['price', 'minimum_nights', 'number_of_reviews','availability_365', 'calculated_host_listings_count', 'number_of_reviews_ltm']].describe())
 
+# Save the concatenated AirBnB listings for christchurch from Oct-2025 to June-2026
 df_chch_all.to_csv("listings_concat.csv")
 
-plt.hist(df_chch_all[df_chch_all['price']<=3000]['price'], bins=80)
+# Distribution(histogram) of price (>= 3000)
 
+plt.hist(df_chch_all[df_chch_all['price']<=3000]['price'], bins=80)
+plt.title("Distribution Of Price")
 plt.show()
+
+# The number of days since last review
+
+     # A publish date column created. 
+     # Since some of the values for the number of days since last review were negative(ranging from -13 to -1), the publish datead was justed .
+
+conditions = [df_chch_all["month_year"] == "June-2026", df_chch_all["month_year"] == "May-2026",
+df_chch_all["month_year"] =="April-2026",
+df_chch_all["month_year"] == "March-2026", df_chch_all["month_year"] == "Feb-2026", 
+df_chch_all["month_year"] == "Jan-2026", df_chch_all["month_year"]== "Dec-2025",
+df_chch_all["month_year"]== "Nov-2025"]
+
+choice = ["2026-06-22", "2026-06-03", "2026-04-23", "2026-03-23", "2026-02-16", 
+"2026-01-24", "2025-12-13", "2025-11-21"]
+
+df_chch_all["publish_date"] = np.select(conditions, choice, default= "2025-10-09")
+
+# last review and publish date columns are converted to datetime data type
+df_chch_all["last_review"] = pd.to_datetime(df_chch_all["last_review"])
+df_chch_all["publish_date"] = pd.to_datetime(df_chch_all["publish_date"])
+df_chch_all.info()
+
+# number of days since last review
+df_chch_all["num_days_since_last_review"] = (df_chch_all["publish_date"]-df_chch_all["last_review"]).dt.days
+
+# distribution of the number of days since last review(histogram)
+plt.hist(df_chch_all['num_days_since_last_review'], bins = 80)
+plt.title("distribution of nimber of days since last review")
+plt.show()
+
+# Top 10 percent highest numbers of reviews
+
+review_threshold = df_chch_all["number_of_reviews"].quantile(0.90)
+top_10_reviews = df_chch_all[df_chch_all["number_of_reviews"] >= review_threshold ].copy()
+top_10_reviews = top_10_reviews.sort_values(by = "number_of_reviews", ascending = False)
+
+print(top_10_reviews[["id", "name", "number_of_reviews", "month_year", "price"]])
+
+print("Top 10 percent highest numbers of reviews", len(top_10_reviews))
