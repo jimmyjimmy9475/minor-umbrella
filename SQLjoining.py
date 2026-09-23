e1bbc758a7fe1be077f2f1f1b0de6ec63dc25a03
Limaz72 <muzammilmahmoodkhan123@gmail.com>
2026-09-23 13:43:51 +1200

    Removed Mock data and titles, replaced with actual dataset

--- SQLjoining.py
+++ SQLjoining.py
@@ -10,31 +10,10 @@ df_airbnb = pd.read_csv('listings_concat_clean.csv')
 df_tenancy = pd.read_csv('tenancy_clean.csv')
 
 
-# Using mock data for template/troubleshooting before applying actual dataframes - 
-# Following code based on template, needs to be adapted for actual dataframes and column names and join structure.
-mock_airbnb = {
-    'id': [1, 2],
-    'sa2': ['326600', '326600'], # Christchurch Central
-    'time_period': ['2026-01', '2026-01'],
-    'price': [200, 250],
-    'beds': [2, 3]
-    }
-
-mock_tenancy = {
-    'sa2': ['326600'],
-    'time_period': ['2026-01'],
-    'price': [150], # Weekly rent index
-    'property_count': [45]
-}
-
-# Turn the mock data into DataFrames
-df_airbnb = pd.DataFrame(mock_airbnb)
-df_tenancy = pd.DataFrame(mock_tenancy)
-
 #Push to SQLite tables
 df_airbnb.to_sql('raw_airbnb', conn, if_exists='replace', index=False)
 df_tenancy.to_sql('raw_tenancy', conn, if_exists='replace', index=False)
-print("Success: SQLite tables created using Mock data.")
+print("Success: SQLite tables created using Cleaned AirBnB and Tenancy Data.")
 
 ## SQL Join Query
 cursor.execute("DROP TABLE IF EXISTS combined_listings_master;")
@@ -43,16 +22,15 @@ sql_join = """
 CREATE TABLE combined_listings_master AS
 SELECT 
     a.id,
-    a.sa2,
-    a.time_period,
+    a.neighbourhood,
+    a.month_year,
     a.price AS airbnb_price,
-    a.beds,
-    t.price AS bond_price,
-    t.property_count AS bond_property_count
+    t.[Median Rent] AS bond_price,
+    t.[Active Bonds] AS bond_property_count
 FROM raw_airbnb a
 LEFT JOIN raw_tenancy t 
-    ON a.sa2 = t.sa2 
-   AND a.time_period = t.time_period;
+    ON a.neighbourhood = t.[Location Id] 
+   AND a.month_year = t.TimeFrame;
 """
 cursor.execute(sql_join)
 print("Success: SQL Left Join completed.")
